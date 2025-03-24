9ec417284b4f7ec9dbd5fd3e0956d4b26d7e85eb
MarsalShyam <shyam12marsal@gmail.com>
2025-03-24 23:21:46 +0530

    frontend and backend first initialization

---
+++ frontend/src/pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <h3 className='font-bold text-center container border'>home</h3>
+    </div>
+  )
+}
+
+export default Home

38b33a0f346d771cb68c65b7b441d81c81ac261b
MarsalShyam <shyam12marsal@gmail.com>
2025-04-24 19:10:03 +0530

    searchbar added in navbar

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,16 +1,33 @@
-import React from 'react'
+import React,{useState} from 'react'
 import ProfileInfo from '../Cards/ProfileInfo'
+import {useNavigate} from "react-router-dom"
+import SearchBar from '../SearchBar/SearchBar';
+
 const Navbar = () => {
+  const [serchQuery,setSearchQuery]=useState("");
+  const navigate=useNavigate;
+
+  const handleSearch=()=>{};
+  const onClearSearch=()=>{
+    setSearchQuery("");
+  }
+
+  const onLogout=()=>{
+    navigate('/login');
+  };
+
   return (
    
-      // <div className="bg-white flex items-center justify-betweeen px-6 py-2 drop-shadow">
-      //   <h2 className="text-xl font-medium text-black py-2">Notes</h2>
-      //   {/* <ProfileInfo/> */}
-      //   ram
-      // </div>
       <div className='flex items-center justify-between bg-white px-6 py-2 drop-shadow'>
         <h2 className="text-xl font-medium text-black py-2">Notes</h2>
-        <ProfileInfo/>
+        <SearchBar value={serchQuery} 
+        onChange={({target})=>{
+          setSearchQuery(target.value);
+        }}
+        handleSearch={handleSearch}
+        onClearSearch={onClearSearch}
+        />
+        <ProfileInfo onLogout={onLogout}/>
       </div>
   )
 }
